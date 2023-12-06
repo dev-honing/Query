1831f8b921f4e8fed309c91812b44bc86f301e19
Ho Nyeong <dev.honing@gmail.com>
2023-12-06 17:07:54 +0900

    index.html 작성하고 GET 요청 처리

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 // app.js
 
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = 3000;
 
-// 정적 파일들을 제공하기 위한 미들웨어
-app.use(express.static('public')); 
-
 // 루트 경로에 대한 요청 핸들러
 app.get('/', (req, res) => {
-  res.send('안녕하세요! Express 서버에 오신 것을 환영합니다.');
+  const indexPath = path.join(__dirname, 'index.html')
+  res.sendFile(indexPath);
 });
 
 // 서버를 지정한 포트로 시작
